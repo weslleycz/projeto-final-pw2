264c5271ab56dca05040b85833933eb842f5e54b
weslley <weslleysouza22@gmail.com>
2023-06-15 19:52:01 -0300

    🔧 Configurando o swagger.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './services/prisma.service';
+import { UserController } from './controllers/user/user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('EcoConnect API')
+    .setDescription(
+      ' EcoConnect é uma plataforma online que conecta pessoas apaixonadas pelo meio ambiente e promove a conscientização ambiental de forma colaborativa. Nossa rede social oferece um espaço interativo para compartilhar informações, histórias inspiradoras, dicas práticas e projetos sustentáveis. Junte-se a uma comunidade engajada, conheça pessoas com interesses semelhantes e descubra maneiras de fazer a diferença para o nosso planeta. Seja parte do movimento e conecte-se com a sustentabilidade através do EcoConnect.',
+    )
+    .setVersion('1.0')
+    .addTag('User')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document, {
+    swaggerOptions: {
+      docExpansion: 'none',
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
